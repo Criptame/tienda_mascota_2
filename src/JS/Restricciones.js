33267da62d07cc5184122d85560cfae53d634267
Guillermo <guillermosantander343@gmail.com>
2025-10-27 21:08:54 -0300

    Jose jose

---
+++ src/JS/Restricciones.js
@@ -0,0 +1,36 @@
+
+document.getElementById("correo").addEventListener("input", function () {
+    const correo = this.value;
+    const errorMsg = document.getElementById("correoError");
+
+    if (!correo.includes("@")) {
+        errorMsg.textContent = "El correo debe contener '@', '.com', '.cl' o ingresar un correo valido.";
+        errorMsg.style.color = "red";
+    } else {
+        errorMsg.textContent = "";
+    }
+});
+
+
+document.getElementById("contraseña").addEventListener("input", function () {
+    const contraseña = this.value;
+    const errorMsg = document.getElementById("contraseñaError");
+
+    if (clave.length < 8) {
+        errorMsg.textContent = "La clave debe tener al menos 8 caracteres.";
+        errorMsg.style.color = "red";
+    } else {
+        errorMsg.textContent = "";
+    }
+});
+
+
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+    const correo = document.getElementById("correo").value;
+    const clave = document.getElementById("contraseña").value;
+
+    if (!correo.includes("@") || clave.length < 8) {
+        e.preventDefault(); 
+        alert("Por favor, corrige los errores antes de continuar.");
+    }
+});
